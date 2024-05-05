0a43b660b2ad5a1363725936dff00937f4e949e5
Mark Small <mark.small@astrosat.net>
2024-05-05 23:52:33 +0100

    feat(project): Initial project setup
    Added code to manage building Github releases using semantic-versioning.

---
+++ release.config.js
@@ -0,0 +1,19 @@
+const branches = require("semantic-release/lib/branches");
+
+module.exports = {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        message: "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+  preset: "angular",
+  branches: ["main"],
+};
